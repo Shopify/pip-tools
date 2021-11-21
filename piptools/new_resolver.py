381d9472d6a5aca9d3f7dc2ac7d5a922ff6db40e
Albert Tugushev <albert@tugushev.ru>
2021-11-21 16:26:54 +0700

    Cleanup

--- piptools/new_resolver.py
+++ piptools/new_resolver.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, Iterable, Set
+from typing import Any, Dict, Iterable, Optional, Set
 
 from pip._internal.cache import WheelCache
 from pip._internal.req import InstallRequirement
@@ -6,6 +6,8 @@ from pip._internal.req.req_tracker import (
     get_requirement_tracker,
     update_env_context_manager,
 )
+from pip._internal.resolution.base import BaseResolver
+from pip._internal.resolution.resolvelib.base import Candidate
 from pip._internal.utils.logging import indent_log
 from pip._internal.utils.temp_dir import TempDirectory, global_tempdir_manager
 from pip._vendor.packaging.specifiers import SpecifierSet
@@ -79,53 +81,69 @@ class NewResolver:
             self.command.trace_basic_info(self.finder)
 
             resolver.resolve(
-                self.constraints, check_supported_wheels=not self.options.target_dir
+                root_reqs=self.constraints,
+                check_supported_wheels=not self.options.target_dir,
             )
 
-        # FIXME: a dirty hack, there must be a better way to get resolved versions
+        return self._get_install_requirements(resolver)
+
+    def _get_install_requirements(
+        self, resolver: BaseResolver
+    ) -> Set[InstallRequirement]:
+        """Returns a set of install requirements from resolver results."""
         reqs = set()
-        for name, candidate in resolver._result.mapping.items():
-            ireq = candidate.get_install_requirement()
+        for candidate in resolver._result.mapping.values():
+            ireq = self._get_install_requirement_from_candidate(
+                resolver=resolver,
+                candidate=candidate,
+            )
             if ireq is None:
                 continue
-
-            version_pin_operator = "=="
-            version_as_str = str(candidate.version)
-            for specifier in ireq.specifier:
-                if specifier.operator == "===" and specifier.version == version_as_str:
-                    version_pin_operator = "==="
-                    break
-
-            ireq.req.specifier = SpecifierSet(
-                f"{version_pin_operator}{candidate.version}"
-            )
-
-            # FIXME: use graph in output writer?
-            required_by = tuple(
-                parent_name
-                for parent_name in resolver._result.graph.iter_parents(name)
-                if parent_name is not None
-            )
-
-            # Filter out unsafe requirements. This logic is incomplete, as it would
-            # fail to filter sub-sub-dependencies of unsafe packages. None of the
-            # UNSAFE_PACKAGES currently have any dependencies at all (which makes sense
-            # for installation tools) so this seems sufficient.
-            if not self.allow_unsafe and ireq.name in UNSAFE_PACKAGES:
-                self.unsafe_constraints.add(ireq)
-                continue
-
-            source_ireq = self._constraints_map.get(key_from_ireq(ireq))
-            if source_ireq is not None and (
-                not hasattr(source_ireq, "_is_existing_pin")
-                or not source_ireq._is_existing_pin
-            ):
-                ireq._source_ireqs = [source_ireq]
-
-            ireq._required_by = required_by
             reqs.add(ireq)
-
         return reqs
+
+    def _get_install_requirement_from_candidate(
+        self, resolver: BaseResolver, candidate: Candidate
+    ) -> Optional[InstallRequirement]:
+        ireq = candidate.get_install_requirement()
+        if ireq is None:
+            return None
+
+        # Filter out unsafe requirements. This logic is incomplete, as it would
+        # fail to filter sub-dependencies of unsafe packages. None of the
+        # UNSAFE_PACKAGES currently have any dependencies at all (which makes sense
+        # for installation tools) so this seems sufficient.
+        if not self.allow_unsafe and ireq.name in UNSAFE_PACKAGES:
+            self.unsafe_constraints.add(ireq)
+            return None
+
+        # Detect pin operator
+        version_pin_operator = "=="
+        version_as_str = str(candidate.version)
+        for specifier in ireq.specifier:
+            if specifier.operator == "===" and specifier.version == version_as_str:
+                version_pin_operator = "==="
+                break
+
+        # Override version specifier
+        ireq.req.specifier = SpecifierSet(f"{version_pin_operator}{candidate.version}")
+
+        # Prepare install requirement parents for annotation
+        ireq._required_by = tuple(
+            parent_name
+            for parent_name in resolver._result.graph.iter_parents(candidate.name)
+            if parent_name is not None
+        )
+
+        # Prepare install requirement sources for annotation
+        source_ireq = self._constraints_map.get(key_from_ireq(ireq))
+        if source_ireq is not None and (
+            not hasattr(source_ireq, "_is_existing_pin")
+            or not source_ireq._is_existing_pin
+        ):
+            ireq._source_ireqs = [source_ireq]
+
+        return ireq
 
     def resolve_hashes(
         self, ireqs: Set[InstallRequirement]
